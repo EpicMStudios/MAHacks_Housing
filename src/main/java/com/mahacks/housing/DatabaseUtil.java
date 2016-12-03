d201d4accbd1abbd2786bd45bf85685c03e044be
speedboy <speedboy@localhost.localdomain>
2016-12-03 18:20:25 -0500

    fsck this

--- src/main/java/com/mahacks/housing/DatabaseUtil.java
+++ src/main/java/com/mahacks/housing/DatabaseUtil.java
@@ -5,34 +5,35 @@ public class DatabaseUtil
    public static Connection connect(String path)
      {
 	Connection c = null;
-	    try 
+	try 
 	  {
-	           Class.forName("org.sqlite.JDBC");
-	           c = DriverManager.getConnection("jdbc:sqlite:"+path); //test.db
+	     Class.forName("org.sqlite.JDBC");
+	     c = DriverManager.getConnection("jdbc:sqlite:"+path); //test.db
 	  }
-	 catch ( Exception e ) 
+	catch ( Exception e ) 
 	  {
-	           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	           System.exit(0);
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
-
-	    System.out.println("Opened database successfully");
+	
+	System.out.println("Opened database successfully");
 	return c;
      }
-
+   
    public static void createTable(String dbPath)
      {
 	Connection c = null;
-	    Statement stmt = null;
-	    try 
+	Statement stmt = null;
+	try 
 	  {
-
+	     
 	     Class.forName("org.sqlite.JDBC");
 	     c = connect(dbPath);
-
+	     
 	     stmt = c.createStatement();
 	     String sql = "CREATE TABLE Applicants " +
-	       " (firstName text NOT NULL, " +
+	       " (id int NOT NULL, " +
+	       " firstName text NOT NULL, " +
 	       " middleName text NOT NULL, " +
 	       " lastName text NOT NULL, " +
 	       " sex int NOT NULL, " +
@@ -49,237 +50,322 @@ public class DatabaseUtil
 	       " email text NOT NULL, " +
 	       " emergencyHousing integer NOT NULL, " +
 	       " preference integer NOT NULL, " +
-	       " ssn text NOT NULL, ";
+	       " ssn text NOT NULL, "
+	       " timeOnWaitingList int NOT NULL";
 	     stmt.executeUpdate(sql);
 	     stmt.close();
 	     c.close();
 	  }
-	 catch ( Exception e ) 
+	catch ( Exception e ) 
 	  {
-
+	     
 	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	     System.exit(0);
 	  }
-
-	    System.out.println("Table created successfully");
+	
+	System.out.println("Table created successfully");
 	
      }
    
-   public static void insertInto(String dbPath, String table, String firstName, String middleName, String lastName, int sex, int race, int isHispanic, int housingType, String disability, String veteran, String income, String address, String mailingAddress, String phone, String workPhone, String email, int emergencyHousing, int preference, String ssn)
+   public static void insertInto(String dbPath, String table, int id, String firstName, String middleName, String lastName, int sex, int race, int isHispanic, int housingType, String disability, String veteran, String income, String address, String mailingAddress, String phone, String workPhone, String email, int emergencyHousing, int preference, String ssn, int timeOnWaitingList)
      {
 	
 	Connection c = null;
-	    Statement stmt = null;
-	    try 
+	Statement stmt = null;
+	try 
 	  {
-
-	           Class.forName("org.sqlite.JDBC");
-	           c = connect(dbPath);
-	           c.setAutoCommit(false);
-
-	           stmt = c.createStatement();
-	           String sql = "INSERT INTO "+table+"(firstName, middleName, lastName, sex, race, isHispanic, housingType, disability, veteran, income, address, mailingAddress, phone, workPhone, email, emergencyHousing, preference, ssn) " +
-	                          "VALUES ("+firstName+","+middleName+","+lastName+","+sex+","+race+","+isHispanic+","+housingType+","+disability+","+veteran+","+income+","+address+","+mailingAddress+","+phone+","+workPhone+","+email+","+emergencyHousing+","+preference+","+ssn+");";
-	           stmt.executeUpdate(sql);
-
-	           stmt.close();
-	           c.commit();
-	           c.close();
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbPath);
+	     c.setAutoCommit(false);
+	     
+	     stmt = c.createStatement();
+	     String sql = "INSERT INTO "+table+"(firstName, middleName, lastName, sex, race, isHispanic, housingType, disability, veteran, income, address, mailingAddress, phone, workPhone, email, emergencyHousing, preference, ssn, timeOnWaitingList) " +
+	       "VALUES ("+firstName+","+middleName+","+lastName+","+sex+","+race+","+isHispanic+","+housingType+","+disability+","+veteran+","+income+","+address+","+mailingAddress+","+phone+","+workPhone+","+email+","+emergencyHousing+","+preference+","+ssn+");";
+	     stmt.executeUpdate(sql);
+	     
+	     stmt.close();
+	     c.commit();
+	     c.close();
 	  }
-	 catch ( Exception e ) 
+	catch ( Exception e ) 
 	  {
-
-	           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	           System.exit(0);
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
-
-	    System.out.println("Records created successfully");
+	
+	System.out.println("Records created successfully");
 	
      }   
    public static void setAtoBWhereCIsD(String dbpath, String table, String a, String b, String c, String d)
      {
-
+	
 	Connection c = null;
-	    Statement stmt = null;
-	    try 
+	Statement stmt = null;
+	try 
 	  {
-
-	           Class.forName("org.sqlite.JDBC");
-	           c = connect(dbpath);
-	           c.setAutoCommit(false);
-
-	           stmt = c.createStatement();
-	           String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
-	           stmt.executeUpdate(sql);
-	           c.commit();
-
-	           ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-
-	           rs.close();
-	           stmt.close();
-	           c.close();
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbpath);
+	     c.setAutoCommit(false);
+	     
+	     stmt = c.createStatement();
+	     String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     //rs.close();
+	     stmt.close();
+	     c.close();
 	  }
-	 catch ( Exception e ) 
+	catch ( Exception e ) 
 	  {
-
-	           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	           System.exit(0);
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
-
-	    System.out.println("Operation done successfully");
+	
+	System.out.println("Operation done successfully");
 	
      }
-
+   
    public static void setAtoBWhereCIsD(String dbpath, String table, String a, int b, String c, int d)
      {
-
-
-	        Connection c = null;
-	            Statement stmt = null;
-	            try
+	
+	
+	Connection c = null;
+	Statement stmt = null;
+	try
 	  {
-
-
-	                        Class.forName("org.sqlite.JDBC");
-	                        c = connect(dbpath);
-	                        c.setAutoCommit(false);
-
-	                        stmt = c.createStatement();
-	                        String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
-	                        stmt.executeUpdate(sql);
-	                        c.commit();
-
-	                        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-
-	                        rs.close();
-	                        stmt.close();
-	                        c.close();
+	     
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbpath);
+	     c.setAutoCommit(false);
+	     
+	     stmt = c.createStatement();
+	     String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     //rs.close();
+	     stmt.close();
+	     c.close();
 	  }
-
-	         catch ( Exception e )
+	
+	catch ( Exception e )
 	  {
-
-
-	                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	                        System.exit(0);
+	     
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
+	
 
-
-	            System.out.println("Operation done successfully");
-
+	System.out.println("Operation done successfully");
+	
      }
-
+   
    public static void setAtoBWhereCIsD(String dbpath, String table, String a, String b, String c, int d)
      {
-
-
-	        Connection c = null;
-	            Statement stmt = null;
-	            try
+	
+	
+	Connection c = null;
+	Statement stmt = null;
+	try
 	  {
-
-
-	                        Class.forName("org.sqlite.JDBC");
-	                        c = connect(dbpath);
-	                        c.setAutoCommit(false);
-
-	                        stmt = c.createStatement();
-	                        String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
-	                        stmt.executeUpdate(sql);
-	                        c.commit();
-
-	                        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-
-	                        rs.close();
-	                        stmt.close();
-	                        c.close();
+	     
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbpath);
+	     c.setAutoCommit(false);
+	     
+	     stmt = c.createStatement();
+	     String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     //rs.close();
+	     stmt.close();
+	     c.close();
 	  }
-
-	         catch ( Exception e )
+	
+	catch ( Exception e )
 	  {
-
-
-	                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	                        System.exit(0);
 	  }
-
-
-	            System.out.println("Operation done successfully");
-
+	
+	
+	System.out.println("Operation done successfully");
+	
      }
-
+   
    public static void setAtoBWhereCIsD(String dbpath, String table, String a, int b, String c, String d)
      {
-
-
-	        Connection c = null;
-	            Statement stmt = null;
-	            try
+	
+	
+	Connection c = null;
+	Statement stmt = null;
+	try
 	  {
-
-
-	                        Class.forName("org.sqlite.JDBC");
-	                        c = connect(dbpath);
-	                        c.setAutoCommit(false);
-
-	                        stmt = c.createStatement();
-	                        String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
-	                        stmt.executeUpdate(sql);
-	                        c.commit();
-
-	                        ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-
-	                        rs.close();
+	     
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbpath);
+	     c.setAutoCommit(false);
+	     
+	     stmt = c.createStatement();
+	     String sql = "UPDATE "+ table +" set "+ a +"="+ b +" where "+ c +"="+ d +" ;";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     //rs.close();
 	                        stmt.close();
-	                        c.close();
+	     c.close();
 	  }
 
-	         catch ( Exception e )
+	catch ( Exception e )
 	  {
-
-
-	                        System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	                        System.exit(0);
+	     
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
-
-
-	            System.out.println("Operation done successfully");
-
+	
+	
+	System.out.println("Operation done successfully");
+	
      }
-
+   
    public static void deleteWhereAIsB(String dbPath, String table, String a, String b)
      {
 	Connection c = null;
 	    Statement stmt = null;
-	    try 
+	try 
 	  {
-
-	           Class.forName("org.sqlite.JDBC");
-	           c = connect(dbPath); 
-	           c.setAutoCommit(false);
-	           //System.out.println("Opened database successfully");
-
-	           stmt = c.createStatement();
-	           String sql = "DELETE from "+ table +" where "+a+"="+b+";";
-	           stmt.executeUpdate(sql);
-	           c.commit();
-
-	           ResultSet rs = stmt.executeQuery( "SELECT * FROM COMPANY;" );
-
-	           rs.close();
-	           stmt.close();
-	           c.close();
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbPath); 
+	     c.setAutoCommit(false);
+	     //System.out.println("Opened database successfully");
+	     
+	     stmt = c.createStatement();
+	     String sql = "DELETE from "+ table +" where "+a+"="+b+";";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     //rs.close();
+	     stmt.close();
+	     c.close();
 	  }
-	 catch ( Exception e ) 
+	catch ( Exception e ) 
 	  {
-
-	           System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	           System.exit(0);
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
 	  }
-
-	    System.out.println("Operation done successfully");
+	
+	System.out.println("Operation done successfully");
+	
+	
      }
    
+   public static void deleteWhereAIsB(String dbPath, String table, String a, int b)
+     {
+	
+	Connection c = null;
+	Statement stmt = null;
+	try
+	  {
+	     
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbPath);
+	     c.setAutoCommit(false);
+	     //System.out.println("Opened database successfully");
+	     
+	     stmt = c.createStatement();
+	     String sql = "DELETE from "+ table +" where "+a+"="+b+";";
+	     stmt.executeUpdate(sql);
+	     c.commit();
+	     
+	     //ResultSet rs = stmt.executeQuery( "SELECT * FROM "+table+";" );
+	     
+	     rs.close();
+	     stmt.close();
+	     c.close();
+	  }
+	
+	catch ( Exception e )
+	  {
+	     
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
+	  }
+	
+	
+	System.out.println("Operation done successfully");
+	
+	
+	
      }
+   
+   public static Applicant[] getApplicantWhereAIsB(String dbPath, String table, String a, int b)
+     {
+
+	ArrayList<Applicant> applicants = new ArrayList<Applicant>();
+	
+	Connection c = null;
+	Statement stmt = null;
+	try 
+	  {
+	     
+	     Class.forName("org.sqlite.JDBC");
+	     c = connect(dbPath)
+	     c.setAutoCommit(false);
+	     //System.out.println("Opened database successfully");
+	     
+	     stmt = c.createStatement();
+	     ResultSet rs = stmt.executeQuery( "SELECT FROM "+table+" where "+a+"="+b+";" );
+	     while ( rs.next() ) 
+	       {
+
+		  Applicants.add(new Applicant(rs.getInt("id"), rs.getString("firstName"), rs.getString("middleName"), rs.getString("lastName"), rs.getInt("sex"),
+					      rs.getString(" address"), rs.getString(" mailingAddress"), rs.getString("workAddress"), rs.getInt("race"), rs.getInt("isHispanic"),
+					      rs.getString("disability"),String veteranStatus"), int income"), rs.getString(" phone"), rs.getString("workPhone"),
+					      rs.getString("email"), rs.getInt("housingType"), rs.getInt("emergencyHousing"), rs.getInt("preferance"), rs.getString(" ssn"), rs.getInt("timeOnWaitlist"));
+		  
+	       }
+	     
+	     rs.close();
+	     stmt.close();
+	     c.close();
+     }
+	catch ( Exception e ) 
+	  {
+	     
+	     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	     System.exit(0);
+	  }
+	
+	System.out.println("Operation done successfully");
+	return applicants.toArray();
+     }
+   
    
 }
