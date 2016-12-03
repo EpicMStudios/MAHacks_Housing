a92887988ac5c6131d2595da6a26298d83408efb
= <jakob@memeware.net>
2016-12-03 14:02:30 -0500

    Static file

--- src/main/java/com/mahacks/housing/Main.java
+++ src/main/java/com/mahacks/housing/Main.java
@@ -1,12 +1,16 @@
+import java.util.HashMap;
+
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 class Main
 {
 
    public static void main(String args[])
      {
-         get("/hello", (req, res) -> "Hello World");
-	
+         staticFileLocation("/public");
+         get("/", (req, res) -> new ModelAndView(new HashMap<>(), "index.ftl"));
      }
    
 }
